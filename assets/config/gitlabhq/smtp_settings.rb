0365e9d68f2dd13b1eb388a32579e81a6bc03854
Sameer Naik <sameer@damagehead.com>
2014-10-19 10:42:15 +0530

    config: whitespace cleanup

--- assets/config/gitlabhq/smtp_settings.rb
+++ assets/config/gitlabhq/smtp_settings.rb
@@ -13,7 +13,7 @@ if Rails.env.production?
     password: "{{SMTP_PASS}}",
     domain: "{{SMTP_DOMAIN}}",
     authentication: "{{SMTP_AUTHENTICATION}}",
-    openssl_verify_mode:  "{{SMTP_OPENSSL_VERIFY_MODE}}",
+    openssl_verify_mode: "{{SMTP_OPENSSL_VERIFY_MODE}}",
     enable_starttls_auto: {{SMTP_STARTTLS}}
   }
 end
