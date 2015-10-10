2516706e6322083f927f5d4b20aee1e186de8754
Sameer Naik <sameer@damagehead.com>
2015-10-10 11:04:39 +0530

    entrypoint.sh: update permissions of `uploads/`
    Refer #456

--- entrypoint.sh
+++ entrypoint.sh
@@ -841,7 +841,7 @@ chown ${GITLAB_USER}:${GITLAB_USER} ${GITLAB_BACKUP_DIR}
 
 # create the uploads directory
 mkdir -p ${GITLAB_DATA_DIR}/uploads/
-chmod -R u+rwX ${GITLAB_DATA_DIR}/uploads/
+chmod 0750 ${GITLAB_DATA_DIR}/uploads/
 chown ${GITLAB_USER}:${GITLAB_USER} ${GITLAB_DATA_DIR}/uploads/
 
 # create the .ssh directory
@@ -974,8 +974,10 @@ appSanitize () {
   chown -R ${GITLAB_USER}:${GITLAB_USER} ${GITLAB_BUILDS_DIR}
 
   echo "Checking uploads directory permissions..."
-  chmod -R u+rwX ${GITLAB_DATA_DIR}/uploads/
-  chown ${GITLAB_USER}:${GITLAB_USER} -R ${GITLAB_DATA_DIR}/uploads/
+  chmod -R 0750 ${GITLAB_DATA_DIR}/uploads/
+  find ${GITLAB_DATA_DIR}/uploads -type f -exec chmod 0644 {} \;
+  find ${GITLAB_DATA_DIR}/uploads -type d -not -path ${GITLAB_DATA_DIR}/uploads -exec chmod 0755 {} \;
+  chown ${GITLAB_USER}:${GITLAB_USER} ${GITLAB_DATA_DIR}/uploads/
 
   echo "Checking tmp directory permissions..."
   chmod -R u+rwX ${GITLAB_DATA_DIR}/tmp/
