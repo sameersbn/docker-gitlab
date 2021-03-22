af6bfcb7897e002598195e2eaf564499fb225aed
intec_bf <bjoern@fernhomberg.de>
2021-03-22 21:55:05 +0100

    workhorse is now bundled with gitlab

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -3,11 +3,10 @@ set -e
 
 GITLAB_CLONE_URL=https://gitlab.com/gitlab-org/gitlab-foss.git
 GITLAB_SHELL_URL=https://gitlab.com/gitlab-org/gitlab-shell/-/archive/v${GITLAB_SHELL_VERSION}/gitlab-shell-v${GITLAB_SHELL_VERSION}.tar.bz2
-GITLAB_WORKHORSE_URL=https://gitlab.com/gitlab-org/gitlab-workhorse.git
 GITLAB_PAGES_URL=https://gitlab.com/gitlab-org/gitlab-pages.git
 GITLAB_GITALY_URL=https://gitlab.com/gitlab-org/gitaly.git
 
-GITLAB_WORKHORSE_BUILD_DIR=/tmp/gitlab-workhorse
+GITLAB_WORKHORSE_BUILD_DIR=${GITLAB_INSTALL_DIR}/workhorse
 GITLAB_PAGES_BUILD_DIR=/tmp/gitlab-pages
 GITLAB_GITALY_BUILD_DIR=/tmp/gitaly
 
@@ -74,7 +73,6 @@ exec_as_git git -C ${GITLAB_INSTALL_DIR} apply --ignore-whitespace < ${GITLAB_BU
 fi
 
 GITLAB_SHELL_VERSION=${GITLAB_SHELL_VERSION:-$(cat ${GITLAB_INSTALL_DIR}/GITLAB_SHELL_VERSION)}
-GITLAB_WORKHORSE_VERSION=${GITLAB_WORKHOUSE_VERSION:-$(cat ${GITLAB_INSTALL_DIR}/GITLAB_WORKHORSE_VERSION)}
 GITLAB_PAGES_VERSION=${GITLAB_PAGES_VERSION:-$(cat ${GITLAB_INSTALL_DIR}/GITLAB_PAGES_VERSION)}
 
 # download golang
@@ -100,11 +98,9 @@ exec_as_git "PATH=$PATH" make verify setup
 # remove unused repositories directory created by gitlab-shell install
 rm -rf ${GITLAB_HOME}/repositories
 
-# download gitlab-workhorse
-echo "Cloning gitlab-workhorse v.${GITLAB_WORKHORSE_VERSION}..."
-git clone -q -b v${GITLAB_WORKHORSE_VERSION} --depth 1 ${GITLAB_WORKHORSE_URL} ${GITLAB_WORKHORSE_BUILD_DIR}
+# build gitlab-workhorse
+echo "Build gitlab-workhorse"
 make -C ${GITLAB_WORKHORSE_BUILD_DIR} install
-
 # clean up
 rm -rf ${GITLAB_WORKHORSE_BUILD_DIR}
 
@@ -445,3 +441,4 @@ rm -rf /var/lib/apt/lists/*
 # clean up caches
 rm -rf ${GITLAB_HOME}/.cache ${GITLAB_HOME}/.bundle ${GITLAB_HOME}/go
 rm -rf /root/.cache /root/.bundle ${GITLAB_HOME}/gitlab/node_modules
+rm -r /tmp/* 
