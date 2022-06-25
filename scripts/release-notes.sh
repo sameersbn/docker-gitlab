6da7a2a07fb9d2858691cc5d4ae38f632292f023
Steven Achilles <steven.achilles@googlemail.com>
2022-06-25 11:42:26 +0200

    Upgrade GitLab CE to 15.1.0

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -40,6 +40,16 @@ Please note that this version does not yet include any rework as a consequence o
 
 You are kindly invited to provide contributions.
 
+### Version-specific instructions for upgrades
+
+Please consider the version specific upgrading instructions for [GitLab CE 15.0.x](https://docs.gitlab.com/ee/update/#1500) and [GitLab CE 15.1.x](https://docs.gitlab.com/ee/update/#1510)
+
+* Elasticsearch 6.8 is no longer supported. Before you upgrade to GitLab 15.0, update Elasticsearch to any 7.x version.
+* If you run external PostgreSQL, particularly AWS RDS, check you have a PostgreSQL bug fix to avoid the database crashing.
+* The use of encrypted S3 buckets with storage-specific configuration is no longer supported after removing support for using background_upload.
+* The certificate-based Kubernetes integration (DEPRECATED) is disabled by default, but you can be re-enable it through the certificate_based_clusters feature flag until GitLab 16.0.
+* In GitLab 15.1.0, we are switching Rails ActiveSupport::Digest to use SHA256 instead of MD5. This affects ETag key generation for resources such as raw Snippet file downloads. In order to ensure consistent ETag key generation across multiple web nodes when upgrading, all servers must first be upgraded to 15.1.Z before upgrading to 15.2.0 or later.
+
 ## Contributing
 
 If you find this image useful here's how you can help:
