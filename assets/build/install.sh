10a48c1541489e4248253aa1513056f82ddcb9e6
Kazunori Kimura <norytama.k2@gmail.com>
2023-02-22 04:55:17 +0900

    Clarify user=root in supervisord.conf
    to prevent "Supervisor running as root" warning

--- assets/build/install.sh
+++ assets/build/install.sh
@@ -251,6 +251,10 @@ echo "UseDNS no" >> /etc/ssh/sshd_config
 # move supervisord.log file to ${GITLAB_LOG_DIR}/supervisor/
 sed -i "s|^[#]*logfile=.*|logfile=${GITLAB_LOG_DIR}/supervisor/supervisord.log ;|" /etc/supervisor/supervisord.conf
 
+# prevent confusing warning "CRIT Supervisor running as root" by clarify run as root
+#   user not defined in supervisord.conf by default, so just append it after [supervisord] block
+sed -i "/\[supervisord\]/a user=root" /etc/supervisor/supervisord.conf
+
 # move nginx logs to ${GITLAB_LOG_DIR}/nginx
 sed -i \
   -e "s|access_log /var/log/nginx/access.log;|access_log ${GITLAB_LOG_DIR}/nginx/access.log;|" \
